eab2a374c22dc2b91ed494f8bfebc9fdb9b3f73e
aliakbar9889 <themraliakbar@gmail.com>
2024-07-17 17:21:42 +0500

    A.A TODO LIST

---
+++ main.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.yellowBright("                     WELCOME TO A.A TODO LIST APPLICATION                          "));
+console.log("====================================");
+let todolist = [];
+let condition = "true";
+while (condition) {
+    let Addtask = await inquirer.prompt([
+        {
+            name: "tasks",
+            type: "input",
+            message: chalk.blueBright("ENTER YOUR NEW TASK")
+        }
+    ]);
+    todolist.push(Addtask.tasks);
+    console.log(`${Addtask.tasks} SUCCESSFULLY ADDED IN YOUR TODO LIST`);
+    let Addmoretasks = await inquirer.prompt([
+        {
+            name: "moretasks",
+            type: "confirm",
+            message: chalk.blueBright("DO YOU WANT ADD ANY MORE TASK"),
+            default: "false"
+        }
+    ]);
+    condition = Addmoretasks.moretasks;
+}
+console.log("YOUR UPDATED TODO LIST", todolist);
